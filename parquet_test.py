9c78d4276e7a54d096b3173503a9d038dc083c45
Ratson <github@ratson.name>
2019-02-11 22:55:00 +0800

    Do a partial update

--- parquet_test.py
+++ parquet_test.py
@@ -17,11 +17,12 @@ def test_read_write_dataset(tmp_path):
     pq.write_to_dataset(table, root_path=tmp_path,
                         partition_cols=['one', 'two'])
 
+    partition_table_path = next(tmp_path.joinpath('one=-1.0', 'two=foo').glob('*.parquet'))
+    df = pd.DataFrame({
+        'three': [True, False, True]
+    }, index=list('abc'))
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, partition_table_path)
+
     table2 = pq.read_table(tmp_path)
-    assert len(table2) == len(table)
-
-    pq.write_to_dataset(table2, root_path=tmp_path,
-                        partition_cols=['one', 'two'])
-
-    table3 = pq.read_table(tmp_path)
-    assert len(table3) == len(table)
+    assert len(table2) == 5
