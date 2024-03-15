1af90934af5f6450e2ed12b8d48340003ee6fe0c
Ratson <github@ratson.name>
2024-03-15 22:26:25 +0800

    Error when refer to external package

--- flake.nix
+++ flake.nix
@@ -3,10 +3,14 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flakelight.url = "github:nix-community/flakelight";
     flakelight.inputs.nixpkgs.follows = "nixpkgs";
+    emacs-ng = {
+      url = "github:emacs-ng/emacs-ng";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = { flakelight, ... }@inputs:
     flakelight ./. {
       inherit inputs;
-      package = pkgs: pkgs.hello;
+      package = { inputs' }: inputs'.emacs-ng.packages.default;
     };
 }
