ca2bf915f6fe2b4b94d26120da59007e1d717eaf
Ratson <github@ratson.name>
2024-07-01 18:55:34 +0800

    Add flake1#nixosConfigurations.vm2

--- flake1/flake.nix
+++ flake1/flake.nix
@@ -3,8 +3,19 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flakelight.url = "github:nix-community/flakelight";
   };
-  outputs = { flakelight, ... }@inputs:
+  outputs = { flakelight, nixpkgs, self, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in
     flakelight ./. {
       inherit inputs;
+
+      nixosConfigurations.vm2 = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          self.nixosModules.default
+        ];
+      };
     };
 }
--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -ex
 
 nix run ./flake1#hello1
 nix build ./flake1#nixosConfigurations.vm1.config.system.build.vm
+nix build ./flake1#nixosConfigurations.vm2.config.system.build.vm
 
 nix run ./flake2#hello2
 nix build ./flake2#nixosConfigurations.vm2.config.system.build.vm
